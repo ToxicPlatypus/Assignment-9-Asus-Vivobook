70ddc5f8251a4f02251dac846c26b254f680cf2e
ToxicPlatypus <rafidrabbi123@gmail.com>
2022-04-03 23:51:10 +0600

    Link added

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="navigation">
+      <Link to="/"> HOME </Link>
+      <Link to="/home"> HOME </Link>
+      <Link to="/reviews"> REVIEWS </Link>
+      <Link to="/dashboard"> DASHBOARD </Link>
+      <Link to="/blogs"> BLOGS </Link>
+      <Link to="/about"> ABOUT </Link>
+    </div>
+  );
+};
+
+export default Header;
